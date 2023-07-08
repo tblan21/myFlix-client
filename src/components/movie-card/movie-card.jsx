fac04b5587e727cda4f9d5b32e7e83679b677e7f
tblan21 <tmb2225@gmail.com>
2023-07-08 12:00:20 -0500

    add react styling

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,14 +1,17 @@
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
-      <div
-        onClick={() => {
-          onMovieClick(movie);
-        }}
-      >
-        {movie.title}
-      </div>
+      <Card className="h-100">
+        <Card.Body>
+          <Card.Title>{movie.title}</Card.Title>
+          <Card.Text>{movie.author}</Card.Text>
+          <Button onClick={() => onMovieClick(movie)} variant="link">
+            Open
+          </Button>
+        </Card.Body>
+      </Card>
     );
   };
 
