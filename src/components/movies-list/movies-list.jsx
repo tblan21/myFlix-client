b4e0711a0aef2d4aec420b52c696453fea98413c
tblan21 <tmb2225@gmail.com>
2023-08-01 06:44:24 -0500

    fix error

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector } from 'redux';
 import { MovieCard } from '../movie-card/movie-card';
 import { MoviesFilter } from '../movies-filter/movies-filter';
 import { Col, Row } from 'react-bootstrap';
