5d0296abcb816ac992780b83ea58161104a9c84c
tblan21 <tmb2225@gmail.com>
2023-08-02 06:47:59 -0500

    fix errors

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -9,7 +9,7 @@ export const MovieCard = ({ movie, onMovieClick }) => {
         <Card.Body>
           <Card.Title>{movie.title}</Card.Title>
           <Card.Text>{movie.description}</Card.Text>
-          <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
+          <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
             <Button variant="link">Open</Button>
           </Link>
         </Card.Body>
